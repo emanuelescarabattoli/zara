a0d0fa66e0f0054b694c7174caa7d6ebda77d46e
Emanuele Scarabattoli <em.scarabattoli@gmail.com>
2018-12-01 19:17:17 +0100

    Styles for buttons

--- frontend/src/containers/Demo/Demo.js
+++ frontend/src/containers/Demo/Demo.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import Page from "../../components/Page/Page";
-import { Card, CardBody } from "../../components/Card/Card";
+import { Card, CardBody, CardHeader } from "../../components/Card/Card";
 
 import "./Demo.css";
 
@@ -10,94 +10,187 @@ class Counters extends Component {
     return (
       <Page title="Demo">
         <Card>
-          <CardBody>
-            <table>
-              <thead>
-                <th>aaa</th>
-                <th>bbb</th>
-                <th></th>
-              </thead>
-              <tbody>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-                <tr>
-                  <td><a>aaa</a></td>
-                  <td>bbb</td>
-                  <td><a><i class="fas fa-trash-alt"></i></a></td>
-                </tr>
-              </tbody>
-            </table>
-          </CardBody>
+          <CardHeader>
+            <button>Save</button>
+          </CardHeader>
+          <table>
+            <thead>
+              <th>aaa</th>
+              <th>bbb</th>
+              <th />
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a>aaa</a>
+                </td>
+                <td>bbb</td>
+                <td>
+                  <a>
+                    <i class="fas fa-trash-alt" />
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </Card>
-        <button className="plus-button"><i className="fas fa-plus" /></button>
+        <button className="plus-button">
+          <i className="fas fa-plus" />
+        </button>
       </Page>
     );
   }
